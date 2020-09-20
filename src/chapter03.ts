7c217e54c51a5404756e8a6bb159654f2dc6b631
Marc Altmann <marcaltmann@posteo.de>
2020-09-20 23:36:01 +0200

    Add boolean example

--- src/chapter03.ts
+++ src/chapter03.ts
@@ -3,4 +3,14 @@
 
   const a = 100n;
   const b: bigint = BigInt(5);
+
+  const c: any = 100;
+
+  const d: unknown = 30;
+
+  // boolean
+  let e: boolean = true;
+  let f: true = true;
+  let g = true;
+  const h = true;
 })();
